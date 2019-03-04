a2ce89055fcdf0de844cf549e7de2d06f3af552d
Maki Tolentino <maki.tolentino.03@gmail.com>
2019-03-04 11:03:47 +0800

    Updates

---
+++ precache-manifest.5c9d5b1a6328c0debac829a8b3ca03bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fd04535e8c7fb629115",
+    "url": "/alexandria/static/js/runtime~main.9fd04535.js"
+  },
+  {
+    "revision": "25806ac5e69a6aff9748",
+    "url": "/alexandria/static/js/main.25806ac5.chunk.js"
+  },
+  {
+    "revision": "67289b03c617e0927d73",
+    "url": "/alexandria/static/js/2.67289b03.chunk.js"
+  },
+  {
+    "revision": "25806ac5e69a6aff9748",
+    "url": "/alexandria/static/css/main.1d34e6d8.chunk.css"
+  },
+  {
+    "revision": "67289b03c617e0927d73",
+    "url": "/alexandria/static/css/2.3d2cb3c8.chunk.css"
+  },
+  {
+    "revision": "3f4e5379a456a59c8ae3557e2f979c37",
+    "url": "/alexandria/index.html"
+  }
+];
